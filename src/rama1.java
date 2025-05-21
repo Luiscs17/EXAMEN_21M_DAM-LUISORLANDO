5dab43666bd3f951e0948c124b97f94412a70f1d
ccana <ccana@Jarvis>
2025-05-21 11:34:35 +0200

    rama1

---
+++ src/rama1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class rama1 {
+	public static void main(String[] args) {
+        System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce un número para averiguar si es primo o no: ");
+        int numero = scanner.nextInt();
+	}
+}
